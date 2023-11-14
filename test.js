fe8592b3fdbaffb41f0e0742206344598ad63bf7
DrMalchev <dr.delyan.malchev@gmail.com>
2023-11-14 16:00:38 +0200

    First tests within node.js

---
+++ test.js
@@ -0,0 +1,10 @@
+const numbersArray = [1, 2, 3, 4, 5]
+
+let mappedArray = numbersArray.map(num => `Number = ${num}`)
+
+const names = ["John", "Carry", "David", "Maria"]
+
+module.exports = {
+    numbers: mappedArray,
+    names: names
+}
