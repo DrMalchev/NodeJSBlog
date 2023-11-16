448e32bc5bb3cfc280c484f0b22355153a0d3c00
DrMalchev <dr.delyan.malchev@gmail.com>
2023-11-16 14:25:14 +0200

    Routing done

--- app.js
+++ app.js
@@ -5,5 +5,27 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.send('<p>aaaaaaaaa</p>');
+    //res.send('<p>Home</p>');
+    res.sendFile('./views/index.html', { root: __dirname })
 });
+
+app.get('/about', (req, res) => {
+    //res.send('<p>About</p>');
+    res.sendFile('./views/about.html', { root: __dirname })
+});
+
+app.get('/about-us', (req, res) => {
+    //res.send('<p>About</p>');
+    res.redirect('/about')
+});
+
+// handle 404
+// app.use() will fire at every request
+// but only if request comes down that far
+// i.e. non of the above app.get() takes place
+
+app.use((req, res) => {
+    res
+        .status('404')
+        .sendFile('./views/404.html', { root: __dirname })
+})
