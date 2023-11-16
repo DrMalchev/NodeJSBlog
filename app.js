ed4153a89dab34be69f36f76fa5c2885a0d29ee1
DrMalchev <dr.delyan.malchev@gmail.com>
2023-11-16 16:17:17 +0200

    EJS templates ready

--- app.js
+++ app.js
@@ -6,19 +6,26 @@ const app = express();
 app.set('view engine', 'ejs')
 
 app.listen(3000);
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     //res.send('<p>Home</p>');
-    res.render('index')
+    const blogs = [
+        { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+        { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+        { title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+    ];
+
+    res.render('index', { title: 'Home', blogs: blogs })
 });
 
 app.get('/about', (req, res) => {
     //res.send('<p>About</p>');
-    res.render('about')
+    res.render('about', { title: 'About' })
 });
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create')
+    res.render('create', { title: 'Create' })
 })
 
 // handle 404
@@ -29,5 +36,5 @@ app.get('/blogs/create', (req, res) => {
 app.use((req, res) => {
     res
         .status(404)
-        .render('404')
+        .render('404', { title: '404' })
 })
