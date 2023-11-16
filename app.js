265025e91338fe621358fb037fdd1102e67f6f54
DrMalchev <dr.delyan.malchev@gmail.com>
2023-11-16 15:14:24 +0200

    Switching to EJS

--- app.js
+++ app.js
@@ -2,22 +2,24 @@ const express = require('express');
 
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs')
+
 app.listen(3000);
 
 app.get('/', (req, res) => {
     //res.send('<p>Home</p>');
-    res.sendFile('./views/index.html', { root: __dirname })
+    res.render('index')
 });
 
 app.get('/about', (req, res) => {
     //res.send('<p>About</p>');
-    res.sendFile('./views/about.html', { root: __dirname })
+    res.render('about')
 });
 
-app.get('/about-us', (req, res) => {
-    //res.send('<p>About</p>');
-    res.redirect('/about')
-});
+app.get('/blogs/create', (req, res) => {
+    res.render('create')
+})
 
 // handle 404
 // app.use() will fire at every request
@@ -26,6 +28,6 @@ app.get('/about-us', (req, res) => {
 
 app.use((req, res) => {
     res
-        .status('404')
-        .sendFile('./views/404.html', { root: __dirname })
+        .status(404)
+        .render('404')
 })
